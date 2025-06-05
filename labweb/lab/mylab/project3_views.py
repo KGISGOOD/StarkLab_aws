6a9c817ef82cdfacd9de1e87f5bcc13c4e3d8edc
KG <eric0909711680@gmail.com>
2025-06-05 04:21:04 +0000

    這姐原先的chronedriver

--- labweb/lab/mylab/project3_views.py
+++ labweb/lab/mylab/project3_views.py
@@ -10,7 +10,7 @@ import re
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
